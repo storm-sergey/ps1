89b0c636ea04cf55bbdd0e1dd05bdb88732f5a9a
storm-sergey <st.sergey.a@gmail.com>
2022-04-13 19:07:00 +0300

    Create SortFilesByDateAfterRecuvaRecovering.ps1

---
+++ SortFilesByDateAfterRecuvaRecovering.ps1
@@ -0,0 +1,37 @@
+$Src = Read-Host -Prompt "Source"                                                                                                                                                                                                             
+$Dest = Read-Host -Prompt "Destination"
+
+$UniqueName = ""
+Get-ChildItem $Src | Sort-Object -Unique | Foreach-Object -EV Err -EA SilentlyContinue {
+    
+    # Check item is a duplicate
+    if (($UniqueName -eq "") -Or (-Not ($_.Name.StartsWith($UniqueName)))) {
+    
+        # Get date for sorting
+        $LastWriteDate = (Get-Item $_.FullName).LastWriteTime
+        $CreationDate = (Get-Item $_.FullName).CreationTime
+        $Date = if ($LastWriteDate -lt $CreationDate) {$LastWriteDate} else {$CreationDate}
+
+        # Make new path 
+        $YearPath = $Dest + "\" + $Date.Year
+        $YearMonthPath = if ($Date.Month -ge 1 -and $Date.Month -le 13) {
+            $YearPath + "\" + (Get-Culture).DateTimeFormat.GetMonthName($Date.Month)
+        } else {
+            $YearPath + "Other"
+        }
+    
+        # Check the path exist
+        if (-Not (Test-Path $YearMonthPath)) {
+            New-Item -Path $YearMonthPath -ItemType Directory
+        }
+
+        # Fix squere brackets in the path
+        $path = $_.FullName.Replace('[', '``[').Replace(']', '``]')
+        Copy-Item -Path $path -Destination $YearMonthPath -Force
+    
+        # Mem for duplicate checking
+        $UniqueName = $_.Name.Substring(0, $_.Name.LastIndexOf(".")) 
+    }   
+}
+
+Read-Host -Prompt "Done, press enter"
